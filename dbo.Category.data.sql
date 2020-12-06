031fbc1726f023876170310e3872d395cb6d6a70
Kata Gál <kata.gal@stud.uni-corvinus.hu>
2020-12-06 18:14:13 +0100

    commit database

---
+++ dbo.Category.data.sql
@@ -0,0 +1,21 @@
+﻿SET IDENTITY_INSERT [dbo].[Category] ON
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (1, N'Automatic Dishwashing')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (2, N'Hand Dishwashing
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (3, N'Household Cleaners
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (4, N'Insecticide
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (5, N'Softener
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (6, N'Special Detergent
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (7, N'Universal Detergenet
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (8, N'WC Block
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (9, N'WC Cleaner
+')
+INSERT INTO [dbo].[Category] ([CategoryID], [Category_name]) VALUES (10, N'Window Cleaner
+')
+SET IDENTITY_INSERT [dbo].[Category] OFF
